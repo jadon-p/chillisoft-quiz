9d29f411146d0b4c5c81e8b70d676841079f2d94
jadon-p <84479993+jadon-p@users.noreply.github.com>
2023-09-04 22:58:54 +1200

    Implementing Email/Password Login
    Started working on this function. It is not complete yet. It adds user to the firebase project but does nothing after that. There is no verification.

--- public/app.js
+++ public/app.js
@@ -13,6 +13,36 @@ function googleLogin(){
     .catch(console.log)
 }
 
-function emailLogin(){
-    
+
+function emailLogin() {
+    const loginEmail = email.value;
+    const loginPassword = password.value;
+
+    console.log(loginEmail);
+    console.log(loginPassword);
+
+    firebase.auth().signInWithEmailAndPassword(loginEmail,loginPassword)
+    .then((userCredential) => {
+        var user = userCredential.user;
+    })
+    .catch((error)=>{
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    })
+}
+
+function createEmailLogin(){
+    const loginEmail = email.value;
+    const loginPassword = password.value;
+    firebase.auth().createUserWithEmailAndPassword(loginEmail, loginPassword)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
 }
